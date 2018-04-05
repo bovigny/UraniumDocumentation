c5428a19ba6a811c6b2edb983fd569b13c0546e2
cbovigny <cbovigny@wl5s-253-98.unil.ch>
2018-04-05 09:48:58 +0200

    last commit

---
+++ roles/ModulesEnvironnement/templates/module.sh
@@ -0,0 +1,8 @@
+shell=`/bin/basename \`/bin/ps -p $$ -ocomm=\``
+if [ -f /usr/share/Modules/init/$shell ]
+then
+  . /usr/share/Modules/init/$shell
+else
+  . /usr/share/Modules/init/sh
+fi
+export MODULEPATH=$MODULEPATH:/opt/software/modules
